f8c0644a1dfe78cf60575843479318e354486a2e
Lisa Wood <lisa@lisawoodstudio.com>
2026-02-14 20:01:53 +0000

    Fix: narrower text bento (60/40), larger gallery images, restore height

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -369,7 +369,7 @@ export const Hero: React.FC = () => {
         
         {/* TOP SECTION */}
         {/* UPDATED: Changed grid layout to [55fr_45fr] for desktop to make video wider */}
-        <div className="flex flex-col lg:grid lg:grid-cols-[55fr_45fr] gap-4 h-auto md:h-[450px] w-full">
+        <div className="flex flex-col lg:grid lg:grid-cols-[60fr_40fr] gap-4 h-auto md:h-[450px] w-full">
             
             {/* 1. TEXT ANIMATION AREA (White Bento) */}
             {/* UPDATED: Changed order to order-2 (Bottom on Mobile, Right on Desktop) */}
